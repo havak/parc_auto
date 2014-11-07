db2260383ef53cfea44d871adf36b48257f261e5
unknown <havak@TERR-4009-9170.classic.local>
2014-11-07 10:26:28 +0100

    parc_voiture_classe_Agence

---
+++ Parc_Auto/Metier/Agence.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Metier
+{
+    class Agence
+    {
+        /// <summary>
+        /// propriétés 
+        /// </summary>
+        private List<Personne> lesPersonnes;
+        private string nom;
+        private List<Voiture> parcVoitures;
+
+        /// <summary>
+        /// accesseurs et mutateurs
+        /// </summary>
+        /// <returns></returns>
+        public List<Personne> getLesPersonnes()
+        {
+            return lesPersonnes;
+        }
+        public void setLesPersonnes(List<Personne> lesPersonnes)
+        {
+            this.lesPersonnes = lesPersonnes;
+        }
+        public string getNom()
+        {
+            return nom;
+        }
+        public void setNom(string nom)
+        {
+            this.nom = nom;
+        }
+        public List<Voiture> getParcVoitures()
+        {
+            return parcVoitures;
+        }
+        public void setParcVoitures(List<Voiture> parcVoitures)
+        {
+            this.parcVoitures = parcVoitures;
+        }
+
+        /// <summary>
+        /// constructeur vide
+        /// </summary>
+        public Agence() { }
+
+        /// <summary>
+        /// constructeur paramétré
+        /// </summary>
+        /// <param name="lesPersonnes"></param>
+        /// <param name="nom"></param>
+        /// <param name="parcVoitures"></param>
+        public Agence(List<Personne> lesPersonnes, string nom, List<Voiture> parcVoitures)
+        {
+            this.lesPersonnes = lesPersonnes;
+            this.nom = nom;
+            this.parcVoitures = parcVoitures;
+        }
+
+        /// <summary>
+        /// ajout de personnes dans la liste
+        /// </summary>
+        /// <param name="unePersonne"></param>
+        public void AjouterPersonne(Personne unePersonne)
+        {
+            lesPersonnes.Add(unePersonne);
+        }
+
+        /// <summary>
+        /// ajout de voitures dans la liste
+        /// </summary>
+        /// <param name="uneVoiture"></param>
+        public void AjouterVoiture(Voiture uneVoiture)
+        {
+            parcVoitures.Add(uneVoiture);
+        }
+        /// <summary>
+        /// méthode permétant de louer une voiture
+        /// </summary>
+        /// <param name="immatriculation"></param>
+        public void LouerVoiture(string immatriculation)
+        {
+            foreach (Voiture item in parcVoitures)
+            {
+                if (item.getImmatriculation() == immatriculation)
+                    item.setEstLouee(true);
+            }
+        }
+        /// <summary>
+        /// méthode perméttant de rendre la voiture
+        /// </summary>
+        /// <param name="immatriculation"></param>
+        public void RendreVoiture(string immatriculation)
+        {
+            foreach (Voiture item in parcVoitures)
+            {
+                if (item.getImmatriculation() == immatriculation)
+                    item.setEstLouee(false);
+            }
+        }
+    }
+}
---
+++ Parc_Auto/Metier/Voiture.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class Voiture
+    {
+
+        //propriétés de la classe voiture
+        private string categorie;
+        private string dateMiseService;
+        private bool estLouee;
+        private string immatriculation;
+        private string loueur;
+        private string nom;
+        private int puissance;
+
+        /// <summary>
+        /// accesseurs et muttateurs
+        /// </summary>
+        /// <returns></returns>
+        public string getCategorie()
+        {
+            return categorie;
+        }
+        public void setCategorie(string categorie)
+        {
+            this.categorie = categorie;
+        }
+        public string getDateMiseService()
+        {
+            return dateMiseService;
+        }
+        public void setDateMiseService(string dateMiseService)
+        {
+            this.dateMiseService = dateMiseService;
+        }
+        public bool getEstLouee()
+        {
+            return estLouee;
+        }
+        public void setEstLouee(bool estLouee)
+        {
+            this.estLouee = estLouee;
+        }
+        public string getImmatriculation()
+        {
+            return immatriculation;
+        }
+        public void setImmatriculation(string immatriculation)
+        {
+            this.immatriculation = immatriculation;
+        }
+        public string getLoueur()
+        {
+            return loueur;
+        }
+        public void setLoueur(string loueur)
+        {
+            this.loueur = loueur;
+        }
+        public string getNom()
+        {
+            return nom;
+        }
+        public void setNom(string nom)
+        {
+            this.nom = nom;
+        }
+        public int getPuissance()
+        {
+            return puissance;
+        }
+        public void setPuissance(int puissance)
+        {
+            this.puissance = puissance;
+        }
+
+        /// <summary>
+        /// constructeur vide
+        /// </summary>
+        public Voiture()
+        {
+
+        }
+        /// <summary>
+        /// constructeur avec paramètres
+        /// </summary>
+        /// <param name="categorie"></param>
+        /// <param name="dateMiseService"></param>
+        /// <param name="estLouee"></param>
+        /// <param name="immatriculation"></param>
+        /// <param name="loueur"></param>
+        /// <param name="nom"></param>
+        /// <param name="puissance"></param>
+        public Voiture(string categorie, string dateMiseService, bool estLouee, string immatriculation, string loueur, string nom, int puissance)
+        {
+            this.categorie = categorie;
+            this.dateMiseService = dateMiseService;
+            this.estLouee = estLouee;
+            this.immatriculation = immatriculation;
+            this.loueur = loueur;
+            this.nom = nom;
+            this.puissance = puissance;
+        }
+
+    }
+}
